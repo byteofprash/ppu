99e93d780487fea1c9547273097027bb23b0f6e0
Prashanth <prash2912@gmail.com>
2018-12-06 15:50:25 +0100

    git ignore

--- dicts.py
+++ dicts.py
@@ -14,6 +14,20 @@ def max_dict(dic):
     ---------
     list
     """
+    return  [k for k,v in dic.items() if v == max(dic.values())]
+
+def min_dict(dic):
+    """
+    Returns a the max key of a dict as a list. If more than 1 has the max values, returns them all.
+    Parameters
+    ----------
+    dic : dict
+
+    Returns
+    ---------
+    list
+    """
     return  [k for k,v in dic.items() if v == min(dic.values())]
+
 
 
