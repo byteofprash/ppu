a3242c17454b071d0cd949e92413189299d876f1
Prashanth Swaminathan <prash2912@gmail.com>
2019-02-22 13:40:12 +0100

    added massrename to fileoperations

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ppu',
+      version='0.1',
+      description='Prash\'s Python Utilities',
+      url='http://github.com/byteofprash/ppu',
+      author='Prashanth Swaminathan',
+      author_email='prash2912@gmail.com',
+      license='MIT',
+      packages=['ppu'],
+      zip_safe=False)
+
--- strings.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-""" Operations on Strings """
-def string2nums(string,separator=',',dtype=int):
-    vals = [dtype(x.strip()) for x in string.split(separator)]
-    return vals
